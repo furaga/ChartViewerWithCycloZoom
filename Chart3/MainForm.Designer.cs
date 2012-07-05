b1493f553fb4bbf2cb9292089d43546b1554d5a9
furaga <furaga.fukahori@gmail.com>
2012-07-05 23:48:07 +0900

    first commit

---
+++ Chart3/MainForm.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Chart3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.pictureBoxChart = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelValue = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.labelTime);
+            this.splitContainer1.Panel2.Controls.Add(this.labelValue);
+            this.splitContainer1.Size = new System.Drawing.Size(1010, 573);
+            this.splitContainer1.SplitterDistance = 542;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Margin = new System.Windows.Forms.Padding(2);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.splitContainer3);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.pictureBoxChart);
+            this.splitContainer2.Size = new System.Drawing.Size(1010, 542);
+            this.splitContainer2.SplitterDistance = 67;
+            this.splitContainer2.SplitterWidth = 3;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.buttonLeft);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.buttonRight);
+            this.splitContainer3.Size = new System.Drawing.Size(1010, 67);
+            this.splitContainer3.SplitterDistance = 460;
+            this.splitContainer3.TabIndex = 4;
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.buttonLeft.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonLeft.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonLeft.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonLeft.Location = new System.Drawing.Point(0, 0);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(460, 67);
+            this.buttonLeft.TabIndex = 2;
+            this.buttonLeft.Text = "◀";
+            this.buttonLeft.UseVisualStyleBackColor = false;
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.buttonRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRight.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonRight.Location = new System.Drawing.Point(0, 0);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(546, 67);
+            this.buttonRight.TabIndex = 3;
+            this.buttonRight.Text = "▶";
+            this.buttonRight.UseVisualStyleBackColor = false;
+            // 
+            // pictureBoxChart
+            // 
+            this.pictureBoxChart.BackColor = System.Drawing.Color.White;
+            this.pictureBoxChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxChart.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxChart.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBoxChart.Name = "pictureBoxChart";
+            this.pictureBoxChart.Size = new System.Drawing.Size(1010, 472);
+            this.pictureBoxChart.TabIndex = 0;
+            this.pictureBoxChart.TabStop = false;
+            this.pictureBoxChart.SizeChanged += new System.EventHandler(this.OnResize);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(163, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // labelTime
+            // 
+            this.labelTime.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labelTime.Location = new System.Drawing.Point(0, 0);
+            this.labelTime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(163, 28);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "label1";
+            // 
+            // labelValue
+            // 
+            this.labelValue.Dock = System.Windows.Forms.DockStyle.Right;
+            this.labelValue.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelValue.Location = new System.Drawing.Point(715, 0);
+            this.labelValue.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelValue.Name = "labelValue";
+            this.labelValue.Size = new System.Drawing.Size(295, 28);
+            this.labelValue.TabIndex = 1;
+            this.labelValue.Text = "label2";
+            this.labelValue.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 30;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 573);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.PictureBox pictureBoxChart;
+        private System.Windows.Forms.Label labelValue;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Chart3/MainForm.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Chart3
+{
+    public partial class MainForm : Form
+    {
+        // データ
+        struct ChartData
+        {
+            public DateTime Date;
+            public double Money;
+            public ChartData(DateTime date, double val)
+            {
+                Date = date;
+                Money = val;
+            }
+        }
+
+        // 相場データ
+
+        // 拡大率
+        double scale = 1.0;
+
+        // グラフ描画用のペン
+        Pen defaultPen = new Pen(new SolidBrush(Color.Black));
+        Pen grayPen = new Pen(new SolidBrush(Color.Gray));
+        Pen lightPen = new Pen(new SolidBrush(Color.LightGray));
+        Pen cursorPen = new Pen(new SolidBrush(Color.Red));
+
+        // グラフ描画用のブラシ
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush blueBrush = new SolidBrush(Color.Blue);
+
+        // 時間計測用ストップウォッチ
+        //        Stopwatch stopWatch = null;
+
+
+        //-------------------------------------------------------------------------------
+        // コンストラクタ
+        //-------------------------------------------------------------------------------
+        public MainForm()
+        {
+            InitializeComponent();
+
+            MouseWheel += PictureBoxChart_MouseWheel;
+
+            try
+            {
+                //
+                // 相場データの読み込み
+                //
+
+                foreach (var line in File.ReadAllLines("../data.csv"))
+                {
+                    var t = line.TrimEnd(new[] { ',', ' ' }).Split(new[] { ',' });
+
+                    double value;
+
+                    if (t.Length >= 2 && double.TryParse(t[1], out value))
+                    {
+                        var date = DateTime.Parse(t[0]);
+                        sortedDataList.Add(new ChartData(date, value));
+                    }
+                }
+
+                // グラフ画像を生成
+                pictureBoxChart.Image = new Bitmap(pictureBoxChart.Width, pictureBoxChart.Height, PixelFormat.Format32bppArgb);
+
+                // 描画
+                DrawChart();
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        //-------------------------------------------------------------------------------
+        //
+        // イベントハンドラ
+        //
+        //-------------------------------------------------------------------------------
+
+        //-------------------------------------------------------------------------------
+        // マウス
+        //-------------------------------------------------------------------------------
+
+        // マウスホイール
+        void PictureBoxChart_MouseWheel(object sender, MouseEventArgs e)
+        {
+            Zoom(e.Delta / 100 * 1.3f);
+            DrawChart();
+        }
+
+        void Zoom(double dScale)
+        {
+
+            var prevScale = scale;
+
+            scale += dScale;
+
+            if (scale < 1)
+            {
+                scale = 1;
+            }
+
+            // カーソル位置を中心として拡大するように調整
+            var imgWidth = pictureBoxChart.Width - offset.X;
+            var scaleRatio = scale / prevScale;
+            var imgCursorX = cursor.X - offset.X;
+            var left = -graphOffsetUnit * prevScale * imgWidth;
+            graphOffsetUnit = ((imgCursorX - left) * scaleRatio - imgCursorX) / (scale * imgWidth);
+        }
+
+        CycloZoom cycroZoom = new CycloZoom();
+
+        // 画面がリサイズされたら
+        void OnResize(object sender, EventArgs e)
+        {
+            pictureBoxChart.Image = new Bitmap(pictureBoxChart.Width, pictureBoxChart.Height, PixelFormat.Format32bppArgb);
+            DrawChart();
+        }
+
+        bool zooming = false;
+        // タイマー呼び出し
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Ellipse ellipse;
+            int dir;
+            zooming = cycroZoom.CheckZoom(Cursor.Position, out ellipse, out dir);
+
+            label1.Text = zooming.ToString() + " " + "e = " + ellipse.E;
+            label1.Refresh();
+
+            if (zooming)
+            {
+                Zoom(1.3 * -dir);
+            }
+
+
+            // 左移動
+            var pt = splitContainer3.Panel1.PointToScreen(buttonLeft.Location);
+            var r = new Rectangle(pt, buttonLeft.Size);
+            if (r.Contains(Cursor.Position))
+            {
+                graphOffsetUnit -= 0.01 / scale;
+            }
+        
+            // 右移動
+            pt = splitContainer3.Panel2.PointToScreen(buttonRight.Location);
+            r = new Rectangle(pt, buttonRight.Size);
+            if (r.Contains(Cursor.Position))
+            {
+                graphOffsetUnit += 0.01 / scale;
+            }
+
+            DrawChart();
+        }
+
+        //-------------------------------------------------------------------------------
+        //
+        // グラフ描画
+        //
+        //-------------------------------------------------------------------------------
+
+        // グラフ位置のオフセット。左下原点
+        readonly Point offset = new Point(50, 30);
+
+        // 表示する金額の最大・最小値
+        const int MAX_MONEY = 150;
+        const int MIN_MONEY = 70;
+
+        // グラフ中に描画するカーソル位置
+        Point cursor;
+
+        // グラフがどれだけ横にスライドしているかを表す。正規化されている
+        double graphOffsetUnit = 0;
+
+        List<ChartData> sortedDataList = new List<ChartData>();
+        Font font = new Font("MS UI Gothic", 12);
+
+        //-------------------------------------------------------------------------------
+        // 座標計算
+        //-------------------------------------------------------------------------------
+
+        // sortedDataListにおけるインデックスからグラフにおけるX座標を計算
+        float GetX(int index)
+        {
+            double MAX_X = sortedDataList.Count;
+            double MIN_X = 0;
+            double graphWidth = pictureBoxChart.Width - offset.X;
+            double scaleX = graphWidth / (MAX_X - MIN_X) * scale;
+            double graphX = index * scaleX + offset.X;
+            double w = scale * graphWidth;
+            double graphOffset = graphOffsetUnit * w;
+            return (float)(graphX - graphOffset);
+        }
+
+        // 金額からグラフにおけるY座標を計算
+        float GetY(double money)
+        {
+            double imgHeight = pictureBoxChart.Height - offset.Y;
+            double scaleY = imgHeight / (MAX_MONEY - MIN_MONEY);
+            return (float)(imgHeight - (money - MIN_MONEY) * scaleY);
+        }
+
+        // 座標を取得
+        PointF GetPoint(int index, double money) { return new PointF(GetX(index), GetY(money)); }
+
+        //-------------------------------------------------------------------------------
+        // インデックス値計算
+        //-------------------------------------------------------------------------------
+
+        // x座標から、最も近い点のインデックスを取得
+        int GetIndex(int x)
+        {
+            double MAX_X = sortedDataList.Count;
+            double MIN_X = 0;
+            double graphWidth = pictureBoxChart.Width - offset.X;
+            double scaleX = graphWidth / (MAX_X - MIN_X) * scale;
+            double w = scale * graphWidth;
+            double graphOffset = graphOffsetUnit * w;
+            double index = (x + graphOffset - offset.X) / scaleX;
+            return (int)Math.Round(index);
+        }
+
+        //-------------------------------------------------------------------------------
+        // 描画
+        //-------------------------------------------------------------------------------
+
+        // チャートを描画
+        void DrawChart()
+        {
+            if (sortedDataList == null) return;
+
+            var sw = Stopwatch.StartNew();
+
+            // 各描画点の座標を取得
+            var points = sortedDataList.Select((pair, index) => GetPoint(index, pair.Money)).Where(pt => pt.X >= offset.X).ToArray();
+            if (points.Length <= 0) return;
+
+            try
+            {
+                using (var g = Graphics.FromImage(pictureBoxChart.Image))
+                {
+                    // 画面をクリア
+                    g.Clear(Color.White);
+
+                    // 横線と縦軸のメモリ
+                    DrawHorizontalLines(g);
+
+                    // 縦線と横軸のメモリ
+                    DrawVirbatileLines(g);
+
+                    // 折れ線
+                    g.DrawLines(defaultPen, points);
+
+                    // カーソル位置の更新
+                    cursor = pictureBoxChart.PointToClient(Cursor.Position);
+                    cursor.X = cursor.X >= offset.X ? cursor.X : offset.X;
+
+                    // カーソルのX座標に近い点のインデックスを取得
+                    var index = GetIndex(cursor.X);
+                    if (index < 0 || sortedDataList.Count <= index) return;
+
+                    var date = sortedDataList[index].Date;
+                    var money = sortedDataList[index].Money;
+
+                    // 日付・金額を表示
+                    labelValue.Text = string.Format("{0}/{1}/{2}  {3}円", date.Year, date.Month, date.Day, money);
+
+                    // 表示される日付・金額に対応するグラフ位置を直行する２本の赤線で表示
+                    var pt = GetPoint(index, money); 
+                    if (!zooming)
+                    {
+                        g.DrawLine(cursorPen, new Point(offset.X, (int)pt.Y), new Point(pictureBoxChart.Width, (int)pt.Y));
+                        g.DrawLine(cursorPen, new Point((int)pt.X, 0), new Point((int)pt.X, pictureBoxChart.Height - offset.Y));
+                    }
+
+                    // 各点
+                    foreach (var p in points)
+                    {
+                        var brush = p.Y < pt.Y ? redBrush : p.Y > pt.Y ? blueBrush : blackBrush;
+                        g.FillEllipse(brush, new Rectangle((int)p.X - 2, (int)p.Y - 2, 4, 4));
+                    }
+
+                    if (!zooming)
+                    {
+                        // 赤線の交点は大きめに
+                        g.FillEllipse(blackBrush, new Rectangle((int)pt.X - 4, (int)pt.Y - 4, 8, 8));
+                    }
+                }
+            }
+            finally
+            {
+                labelTime.Text = sw.ElapsedMilliseconds + " ms";
+
+                // 再描画
+                pictureBoxChart.Refresh();
+                labelValue.Refresh();
+                labelTime.Refresh();
+            }
+        }
+
+        // 横線と縦軸のメモリの描画
+        void DrawHorizontalLines(Graphics g)
+        {
+            for (int money = (int)MIN_MONEY; money < (int)MAX_MONEY; money++)
+            {
+                int y = (int)GetY(money);
+                if (money % 10 != 0)
+                {
+                    // 金額が10の倍数以外は薄めの線
+                    g.DrawLine(lightPen, new Point(offset.X, y), new Point(pictureBoxChart.Width, y));
+                }
+                else
+                {
+                    // 金額が10の倍数なら濃い目の線
+                    g.DrawLine(grayPen, new Point(offset.X, y), new Point(pictureBoxChart.Width, y));
+
+                    // 金額をグラフの横に表示
+                    string text = money.ToString();
+                    var textSize = g.MeasureString(text, font);
+                    float textX = offset.X - textSize.Width;
+                    float textY = y - textSize.Height / 2;
+                    g.DrawString(text, font, blackBrush, new PointF(textX, textY));
+                }
+            }
+
+            // 一番下側の線（濃い目の線で描画）
+            g.DrawLine(defaultPen, new Point(offset.X, (int)GetY(MIN_MONEY)), new Point(pictureBoxChart.Width, (int)GetY(MIN_MONEY)));
+        }
+
+        // 縦線と横軸のメモリ
+        void DrawVirbatileLines(Graphics g)
+        {
+            var bottom = pictureBoxChart.Height - offset.Y;
+            
+            // 各年最初のデータのリスト
+            List<int> borders = new List<int>();
+
+            int prev = 0;
+            foreach (var data in sortedDataList.Select((pair, Index) => new { Year = pair.Date.Year, Index }))
+            {
+                if (prev != data.Year)
+                {
+                    borders.Add(data.Index);
+                    prev = data.Year;
+                }
+            }
+
+            // 一番左側の線（濃い目の線で描画）
+            g.DrawLine(defaultPen, new Point(offset.X, 0), new Point(offset.X, bottom));
+            for (int i = 0; i < borders.Count - 1; i++)
+            {
+                int left = (int)GetX(borders[i]);
+                int right = (int)GetX(borders[i + 1]);
+
+                // 境界線
+                g.DrawLine(lightPen, new Point(right, 0), new Point(right, bottom));
+
+                // メモリ（年）
+                var text = sortedDataList[borders[i]].Date.Year.ToString();
+                var textWidth = g.MeasureString(text, font).Width;
+                var textX = (left + right - textWidth) / 2;
+                if (textX >= offset.X) g.DrawString(text, font, blackBrush, new PointF(textX, bottom));
+            }
+        }
+
+
+    }
+}
